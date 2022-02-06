e99de1c68bd93876edcd5069437197d9ada2f07e
0/0 <zero@imaskeleton.me>
2022-02-06 00:10:16 -0700

    fix unsatisfied trait bounds in threadpool.rs

--- scripty_audio/src/threadpool.rs
+++ scripty_audio/src/threadpool.rs
@@ -1,18 +1,19 @@
 use once_cell::sync::OnceCell;
+use std::pin::Pin;
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::mpsc;
-use std::sync::mpsc::RecvError;
 use tokio::sync::oneshot;
 use tokio::sync::oneshot::error::RecvError as TokioRecvError;
 
-static THREADPOOL_SUBMIT: OnceCell<mpsc::Sender<Box<dyn FnOnce()>>> = OnceCell::new();
+static THREADPOOL_SUBMIT: OnceCell<mpsc::SyncSender<Pin<Box<dyn FnOnce() + Send + Sync>>>> =
+    OnceCell::new();
 static COMPLETED_JOBS: AtomicU64 = AtomicU64::new(0);
 
 pub fn init_threadpool() {
     let pool = threadpool::Builder::new()
         .num_threads(num_cpus::get() / 2)
         .build();
-    let (tx, rx) = mpsc::channel();
+    let (tx, rx) = mpsc::sync_channel(usize::MAX);
     THREADPOOL_SUBMIT.set(tx);
 
     std::thread::spawn(move || match rx.recv() {
@@ -24,26 +25,28 @@ pub fn init_threadpool() {
     });
 }
 
-pub fn submit_job<T>(f: Box<dyn FnOnce(oneshot::Sender<T>) -> ()>) -> Result<T, TokioRecvError> {
-    let (tx, rx) = oneshot::channel();
-
-    THREADPOOL_SUBMIT
-        .get()
-        .expect("failed to fetch threadpool submitter")
-        .send(Box::new(|| f(tx)));
-
-    rx.blocking_recv()
-}
-
-pub async fn submit_job_async<T>(
-    f: Box<dyn FnOnce(oneshot::Sender<T>) -> ()>,
+pub fn submit_job<T: 'static + Send + Sync>(
+    f: Box<dyn FnOnce(oneshot::Sender<T>) + Send + Sync>,
 ) -> Result<T, TokioRecvError> {
     let (tx, rx) = oneshot::channel();
 
     THREADPOOL_SUBMIT
         .get()
         .expect("failed to fetch threadpool submitter")
-        .send(Box::new(|| f(tx)));
+        .send(Box::pin(|| f(tx)));
+
+    rx.blocking_recv()
+}
+
+pub async fn submit_job_async<T: 'static + Send + Sync>(
+    f: Box<dyn FnOnce(oneshot::Sender<T>) + Send + Sync>,
+) -> Result<T, TokioRecvError> {
+    let (tx, rx) = oneshot::channel();
+
+    THREADPOOL_SUBMIT
+        .get()
+        .expect("failed to fetch threadpool submitter")
+        .send(Box::pin(|| f(tx)));
 
     rx.await
 }
