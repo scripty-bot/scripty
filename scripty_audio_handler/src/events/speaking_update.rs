b746fe0aed002c43c5952fbcdd270b8726181698
0/0 <zero@imaskeleton.me>
2022-02-14 14:16:20 -0700

    remove else block from speaking_update.rs

--- scripty_audio_handler/src/events/speaking_update.rs
+++ scripty_audio_handler/src/events/speaking_update.rs
@@ -19,102 +19,100 @@ pub async fn speaking_update(
 ) {
     let ssrc = update.ssrc;
     debug!(?ssrc, ?update.speaking, "got SpeakingUpdate event");
-    if update.speaking {
-        // user has started speaking, begin a new model
-    } else {
-        // user has finished speaking, run the STT algo
-        let old_stream = match ssrc_stream_map.insert(
-            ssrc,
-            scripty_audio::get_stream("en").expect("en invalid lang?"),
-        ) {
-            Some(s) => s,
-            None => {
-                warn!(?ssrc, "stream not found in ssrc_stream_map, bailing");
-                return;
-            }
-        };
-        debug!(?ssrc, "found Stream for SSRC");
-
-        if let Some(pkts) =
-            ssrc_audio_map.insert(ssrc, (Vec::with_capacity(EXPECTED_PKT_SIZE * 5), 0))
-        {
-            if !pkts.0.is_empty() {
-                debug!(?ssrc, "found audio packets for SSRC, feeding before ending");
-                old_stream.feed_audio_async(pkts.0).await;
-            } else {
-                debug!(?ssrc, "no extra audio packets to feed");
-            }
+    if !update.speaking {
+        return;
+    }
+    // user has finished speaking, run the STT algo
+    let old_stream = match ssrc_stream_map.insert(
+        ssrc,
+        scripty_audio::get_stream("en").expect("en invalid lang?"),
+    ) {
+        Some(s) => s,
+        None => {
+            warn!(?ssrc, "stream not found in ssrc_stream_map, bailing");
+            return;
         }
+    };
+    debug!(?ssrc, "found Stream for SSRC");
 
-        let user_data = ssrc_user_data_map.get(&ssrc);
-        let (username, avatar_url) = match user_data {
-            Some(ref d) => d.value(),
-            None => {
-                warn!(?ssrc, "user data not found in ssrc_user_data_map, bailing");
-                return;
-            }
-        };
-        debug!(?ssrc, "found user data for SSRC");
-
-        let mut webhook_execute = ExecuteWebhook::default();
-
-        debug!(?ssrc, "running transcription");
-        if verbose.load(Ordering::Relaxed) {
-            let res = old_stream.finish_stream_with_metadata_async(3).await;
-            debug!(?ssrc, "ran stream transcription");
-            match res {
-                Ok(res) if res.num_transcripts() != 0 => {
-                    // SAFETY: we have already checked len != 0, so there must be at least one item
-                    let transcript = unsafe { res.transcripts().get_unchecked(0) };
-
-                    webhook_execute.embeds(vec![Embed::fake(|e| {
-                        e.title(format!("Transcript 1/{}", res.num_transcripts()))
-                            .field("Transcription", transcript.to_owned(), false)
-                            .field("Confidence", transcript.confidence(), false)
-                            .footer(|f| f.text(format!("ssrc {}", ssrc)))
-                    })]);
-                }
-                Err(e) => {
-                    error!(?ssrc, "stream transcription errored: {}", e);
-
-                    webhook_execute.content(format!(
-                        "internal error: running stt algorithm failed with error: {}\nssrc {}",
-                        e, ssrc
-                    ));
-                }
-                _ => return,
-            }
+    if let Some(pkts) = ssrc_audio_map.insert(ssrc, (Vec::with_capacity(EXPECTED_PKT_SIZE * 5), 0))
+    {
+        if !pkts.0.is_empty() {
+            debug!(?ssrc, "found audio packets for SSRC, feeding before ending");
+            old_stream.feed_audio_async(pkts.0).await;
         } else {
-            let res = old_stream.finish_stream_async().await;
-            debug!(?ssrc, "ran stream transcription");
+            debug!(?ssrc, "no extra audio packets to feed");
+        }
+    }
 
-            match res {
-                Ok(res) if !res.is_empty() => {
-                    webhook_execute.content(res);
-                }
-                Err(e) => {
-                    error!(?ssrc, "stream transcription errored: {}", e);
+    let user_data = ssrc_user_data_map.get(&ssrc);
+    let (username, avatar_url) = match user_data {
+        Some(ref d) => d.value(),
+        None => {
+            warn!(?ssrc, "user data not found in ssrc_user_data_map, bailing");
+            return;
+        }
+    };
+    debug!(?ssrc, "found user data for SSRC");
 
-                    webhook_execute.content(format!(
-                        "internal error: running stt algorithm failed with error: {}\nssrc {}",
-                        e, ssrc
-                    ));
-                }
-                _ => return,
+    let mut webhook_execute = ExecuteWebhook::default();
+
+    debug!(?ssrc, "running transcription");
+    if verbose.load(Ordering::Relaxed) {
+        let res = old_stream.finish_stream_with_metadata_async(3).await;
+        debug!(?ssrc, "ran stream transcription");
+        match res {
+            Ok(res) if res.num_transcripts() != 0 => {
+                // SAFETY: we have already checked len != 0, so there must be at least one item
+                let transcript = unsafe { res.transcripts().get_unchecked(0) };
+
+                webhook_execute.embeds(vec![Embed::fake(|e| {
+                    e.title(format!("Transcript 1/{}", res.num_transcripts()))
+                        .field("Transcription", transcript.to_owned(), false)
+                        .field("Confidence", transcript.confidence(), false)
+                        .footer(|f| f.text(format!("ssrc {}", ssrc)))
+                })]);
             }
-        }
-        debug!(?ssrc, "stream transcription succeeded");
+            Err(e) => {
+                error!(?ssrc, "stream transcription errored: {}", e);
 
-        webhook_execute.username(username).avatar_url(avatar_url);
-        debug!(?ssrc, "sending webhook msg");
-        let res = webhook
-            .execute(&ctx, false, |e| {
-                *e = webhook_execute;
-                e
-            })
-            .await;
-        if let Err(e) = res {
-            error!(?ssrc, "failed to send webhook msg: {}", e);
+                webhook_execute.content(format!(
+                    "internal error: running stt algorithm failed with error: {}\nssrc {}",
+                    e, ssrc
+                ));
+            }
+            _ => return,
         }
+    } else {
+        let res = old_stream.finish_stream_async().await;
+        debug!(?ssrc, "ran stream transcription");
+
+        match res {
+            Ok(res) if !res.is_empty() => {
+                webhook_execute.content(res);
+            }
+            Err(e) => {
+                error!(?ssrc, "stream transcription errored: {}", e);
+
+                webhook_execute.content(format!(
+                    "internal error: running stt algorithm failed with error: {}\nssrc {}",
+                    e, ssrc
+                ));
+            }
+            _ => return,
+        }
+    }
+    debug!(?ssrc, "stream transcription succeeded");
+
+    webhook_execute.username(username).avatar_url(avatar_url);
+    debug!(?ssrc, "sending webhook msg");
+    let res = webhook
+        .execute(&ctx, false, |e| {
+            *e = webhook_execute;
+            e
+        })
+        .await;
+    if let Err(e) = res {
+        error!(?ssrc, "failed to send webhook msg: {}", e);
     }
 }
