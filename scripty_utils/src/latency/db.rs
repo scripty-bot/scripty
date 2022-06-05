015c136d2748806febe95b56cb740c6fcf83782e
0/0 <zero@imaskeleton.me>
2022-06-05 12:54:57 -0600

    fix issue in database latency

--- scripty_utils/src/latency/db.rs
+++ scripty_utils/src/latency/db.rs
@@ -2,7 +2,9 @@
 pub async fn get_db_latency() -> u128 {
     let db = scripty_db::get_db();
     let st = std::time::Instant::now();
-    let _ = sqlx::query!("SELECT guild_id FROM prefixes LIMIT 1").fetch_one(db);
+    let _ = sqlx::query!("SELECT guild_id FROM prefixes LIMIT 1")
+        .fetch_one(db)
+        .await;
     let et = std::time::Instant::now();
     et.duration_since(st).as_nanos()
 }
