b0747ef4aaf5a8065207971fe6e29495db0f0aba
0/0 <zero@imaskeleton.me>
2022-07-05 19:03:36 -0600

    finally implement pretty language name parsing

--- scripty_i18n/src/lib.rs
+++ scripty_i18n/src/lib.rs
@@ -4,6 +4,7 @@ extern crate tracing;
 mod bundles;
 mod cache;
 mod init;
+mod pretty;
 mod store;
 mod strings;
 
@@ -11,6 +12,7 @@ pub use bundles::*;
 pub use cache::*;
 pub use fluent::FluentArgs;
 pub use init::*;
+pub use pretty::*;
 pub use store::*;
 pub use strings::*;
 pub use unic_langid::LanguageIdentifier;
---
+++ scripty_i18n/src/pretty.rs
@@ -0,0 +1,33 @@
+//! Human-readable representation of ISO-639 language codes.
+use once_cell::sync::OnceCell;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+pub type LanguageMap = HashMap<String, LanguageMapValue>;
+
+#[derive(Serialize, Deserialize)]
+pub struct LanguageMapValue {
+    native: String,
+    english: String,
+}
+
+const LANGUAGE_MAP: &str = include_str!("../locales/codes.json");
+static CACHED_LANGUAGE_MAP: OnceCell<LanguageMap> = OnceCell::new();
+
+pub fn get_language_map() -> &'static LanguageMap {
+    CACHED_LANGUAGE_MAP
+        .get_or_try_init(|| serde_json::from_str(LANGUAGE_MAP))
+        .expect("failed to parse language map")
+}
+
+/// Get the pretty version of a language's name, in both native and English, falling back to the language code.
+pub fn get_pretty_language_name(language: &str) -> (String, String) {
+    CACHED_LANGUAGE_MAP
+        .get_or_try_init(|| serde_json::from_str(LANGUAGE_MAP))
+        .expect("failed to parse language map")
+        .get(language)
+        .map_or_else(
+            || (language.to_string(), language.to_string()),
+            |lang| (lang.native.clone(), lang.english.clone()),
+        )
+}
