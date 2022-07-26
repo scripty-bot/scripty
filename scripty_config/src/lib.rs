70fd583d70aa8dd04363b54506ddbc1bc69e98a7
0/0 <zero@imaskeleton.me>
2022-07-25 23:26:40 -0600

    add compile-time OS checks

--- scripty_config/src/lib.rs
+++ scripty_config/src/lib.rs
@@ -4,5 +4,15 @@ extern crate serde;
 mod cfg;
 mod load;
 
+// we place all checks here that way they're tossed as early in the build as possible
+
+#[cfg(target_os = "windows")]
+compile_error!("Scripty *will not* run on Windows whatsoever. Don't even try.");
+
+#[cfg(not(target_os = "linux"))]
+compile_error!("Scripty is only designed for Linux.\
+ It may not run at all on other platforms.\
+ If you'd like to try anyways, comment out this section of code.");
+
 pub use cfg::*;
 pub use load::*;
