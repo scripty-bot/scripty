4a49aa5853fff7e9a07218f5efa68f8c5ae64fbb
0/0 <zero@imaskeleton.me>
2022-08-02 22:11:08 -0600

    forgot to await

--- scripty_commands/src/cmds/register_cmds.rs
+++ scripty_commands/src/cmds/register_cmds.rs
@@ -5,6 +5,6 @@ use crate::{Context, Error};
 /// Run with no arguments to register in guild, run with argument "global" to register globally.
 #[poise::command(prefix_command, hide_in_help)]
 pub async fn register_cmds(ctx: Context<'_>) -> Result<(), Error> {
-    poise::builtins::register_application_commands_buttons(ctx);
+    poise::builtins::register_application_commands_buttons(ctx).await?;
     Ok(())
 }
