5df2ba52a5261afc1c95254047460e74bceecdc6
0/0 <zero@imaskeleton.me>
2022-08-02 23:56:13 -0600

    fix register_cmds

--- scripty_commands/src/cmds/register_cmds.rs
+++ scripty_commands/src/cmds/register_cmds.rs
@@ -4,7 +4,7 @@ use crate::{Context, Error};
 ///
 /// Run with no arguments to register in guild, run with argument "global" to register globally.
 #[poise::command(prefix_command, hide_in_help)]
-pub async fn register_cmds(ctx: Context<'_>) -> Result<(), Error> {
-    poise::builtins::register_application_commands_buttons(ctx).await?;
+pub async fn register_cmds(ctx: Context<'_>, #[flag] global: bool) -> Result<(), Error> {
+    poise::builtins::register_application_commands(ctx, global).await?;
     Ok(())
 }
