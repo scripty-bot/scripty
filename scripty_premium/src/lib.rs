ad7af0a0b64bf5e9d09b8c25821d468273db1a88
0/0 <zero@imaskeleton.me>
2022-08-02 23:57:07 -0600

    remove premium caching

--- scripty_premium/src/lib.rs
+++ scripty_premium/src/lib.rs
@@ -1,5 +1,3 @@
-use dashmap::DashMap;
-use once_cell::sync::OnceCell;
 use std::num::NonZeroU64;
 use time::OffsetDateTime;
 
@@ -39,39 +37,15 @@ impl From<i16> for PremiumTierList {
     }
 }
 
-static PREMIUM_USER_CACHE: OnceCell<DashMap<NonZeroU64, PremiumUserInfo>> = OnceCell::new();
-
 pub async fn get_user(user_id: NonZeroU64) -> Option<PremiumUserInfo> {
-    let cache = PREMIUM_USER_CACHE.get_or_init(DashMap::new);
-
-    if let Some(mut c) = cache.get_mut(&user_id) {
-        // if the timestamp in the cache is in the past, remove from the database and update the cache to reflect the change
-        if c.expiration
-            .map(|t| t < OffsetDateTime::now_utc())
-            .unwrap_or(false)
-        {
-            let db = scripty_db::get_db();
-            sqlx::query!(
-                "UPDATE users SET premium_level = 0, premium_expiry = NULL WHERE user_id = $1",
-                user_id.get() as i64
-            )
-            .execute(db)
-            .await
-            .expect("Failed to update user");
-
-            c.tier = PremiumTierList::None;
-            c.expiration = None;
-        }
-
-        return Some(*c.value());
-    }
-
     let db = scripty_db::get_db();
 
     let res = sqlx::query!(
-        "SELECT premium_level, premium_expiry, is_trialing, trial_used FROM users WHERE user_id = $1",
+        "SELECT premium_level, premium_expiry, is_trialing FROM users WHERE user_id = $1",
         user_id.get() as i64
-    ).fetch_optional(db).await;
+    )
+    .fetch_optional(db)
+    .await;
 
     match res {
         Ok(Some(r)) => {
@@ -102,51 +76,20 @@ pub async fn get_user(user_id: NonZeroU64) -> Option<PremiumUserInfo> {
                 expiration: premium_expiration,
                 tier: premium_tier,
             };
-            cache.insert(user_id, premium_user_info);
             Some(premium_user_info)
         }
-        Ok(None) => {
-            // create default and insert into cache
-            let premium_user_info = PremiumUserInfo {
-                expiration: None,
-                tier: PremiumTierList::None,
-            };
-            cache.insert(user_id, premium_user_info);
-            Some(premium_user_info)
-        }
+        Ok(None) => Some(PremiumUserInfo {
+            expiration: None,
+            tier: PremiumTierList::None,
+        }),
         Err(e) => {
             error!("Error fetching user info: {}", e);
-            // create default and don't insert into cache
-            Some(PremiumUserInfo {
-                expiration: None,
-                tier: PremiumTierList::None,
-            })
+            None
         }
     }
 }
 
-pub fn update_cached_user(
-    user_id: NonZeroU64,
-    premium_tier: PremiumTierList,
-    expiry: Option<OffsetDateTime>,
-) {
-    let cache = PREMIUM_USER_CACHE.get_or_init(DashMap::new);
-    cache.alter(&user_id, |_, mut info| {
-        info.tier = premium_tier;
-        info.expiration = expiry;
-        info
-    })
-}
-
-static PREMIUM_GUILD_CACHE: OnceCell<DashMap<NonZeroU64, PremiumTierList>> = OnceCell::new();
-
 pub async fn get_guild(guild_id: NonZeroU64) -> Option<PremiumTierList> {
-    let cache = PREMIUM_GUILD_CACHE.get_or_init(DashMap::new);
-
-    if let Some(c) = cache.get_mut(&guild_id) {
-        return Some(*c.value());
-    }
-
     // from here, fetch the user that corresponds to the guild table's premium_owner_id column
     let db = scripty_db::get_db();
     let r = sqlx::query!(
@@ -155,23 +98,11 @@ pub async fn get_guild(guild_id: NonZeroU64) -> Option<PremiumTierList> {
     ).fetch_optional(db).await;
 
     match r {
-        Ok(Some(r)) => {
-            let premium_tier = PremiumTierList::from(r.premium_level);
-            cache.insert(guild_id, premium_tier);
-            Some(premium_tier)
-        }
-        Ok(None) => {
-            cache.insert(guild_id, PremiumTierList::None);
-            Some(PremiumTierList::None)
-        }
+        Ok(Some(r)) => Some(PremiumTierList::from(r.premium_level)),
+        Ok(None) => Some(PremiumTierList::None),
         Err(e) => {
             error!("Error fetching guild info: {}", e);
-            Some(PremiumTierList::None)
+            None
         }
     }
-}
-
-pub fn update_cached_guild(guild_id: NonZeroU64, premium_tier: PremiumTierList) {
-    let cache = PREMIUM_GUILD_CACHE.get_or_init(DashMap::new);
-    cache.insert(guild_id, premium_tier);
 }
