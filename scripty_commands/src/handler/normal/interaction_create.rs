34c782642f3fef2de90855d7b9989b9eb199ac8d
0/0 <zero@imaskeleton.me>
2022-08-29 10:19:06 -0600

    log incoming application command event data

--- scripty_commands/src/handler/normal/interaction_create.rs
+++ scripty_commands/src/handler/normal/interaction_create.rs
@@ -1,6 +1,8 @@
 use serenity::client::Context;
 use serenity::model::application::interaction::Interaction;
 
-pub async fn interaction_create(_ctx: Context, _interaction: Interaction) {
-    // unused for now
+pub async fn interaction_create(_ctx: Context, interaction: Interaction) {
+    if let Some(cmd) = interaction.application_command() {
+        info!("got data {:?}", cmd.data);
+    }
 }
