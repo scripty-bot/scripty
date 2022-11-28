d62c921e0fdcb51993b196ce2f0703defe1dee99
0/0 <zero@imaskeleton.me>
2022-11-27 17:46:52 -0700

    add list_rules for automod

--- scripty_commands/src/cmds/automod/list_rules.rs
+++ scripty_commands/src/cmds/automod/list_rules.rs
@@ -1,6 +1,7 @@
 use super::{get_next_tier, get_tier_rule_count, AutomodRuleAction, AutomodRuleType};
 use crate::{Context, Error};
 use poise::CreateReply;
+use scripty_utils::do_paginate;
 use serenity::builder::CreateEmbed;
 
 #[poise::command(
@@ -8,6 +9,42 @@ use serenity::builder::CreateEmbed;
     slash_command,
     guild_only,
     required_permissions = "MANAGE_GUILD",
-    rename = "add_rule"
+    rename = "list_rules"
 )]
-pub async fn automod_list_rules(ctx: Context<'_>) -> Result<(), Error> {}
+pub async fn automod_list_rules(ctx: Context<'_>) -> Result<(), Error> {
+    // fetch the current guild's rule count
+    let db = scripty_db::get_db();
+    let gid = ctx.guild_id().expect("asserted in guild").0;
+    let resolved_language = scripty_i18n::get_resolved_language(ctx.author().id.0, Some(gid)).await;
+
+    let rules: Vec<_> = sqlx::query!(
+        "SELECT item_id, rule_type, rule_action, rule_data FROM automod_rules WHERE source_id = (SELECT item_id FROM automod_config WHERE guild_id = $1) ORDER BY item_id ASC",
+        gid.get() as i64
+    ).fetch_all(db).await?;
+
+    let mut formatted_rules = rules
+        .into_iter()
+        .map(|x| (
+            format_message!(resolved_language, "automod-list-rules-embed-field-name", ruleId: x.item_id),
+            format_message!(
+                resolved_language,
+                "automod-list-rules-embed-field-value",
+                ruleType: x.rule_type,
+                ruleAction: x.rule_action,
+                ruleContent: x.rule_data
+            )
+        )).collect::<Vec<_>>();
+
+    do_paginate(
+        ctx.discord(),
+        ctx.channel_id(),
+        formatted_rules,
+        format_message!(resolved_language, "automod-list-rules-embed-title"),
+        None,
+        None,
+        Some(ctx.author().id),
+    )
+    .await?;
+
+    Ok(())
+}
