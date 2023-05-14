eac2c85d6a172a7a43b60664d59df5c7ca941766
Zero <zero@imaskeleton.me>
2023-05-13 20:32:14 -0600

    rename to more descriptive names

--- scripty_bot_utils/src/background_tasks/tasks/mod.rs
+++ scripty_bot_utils/src/background_tasks/tasks/mod.rs
@@ -1,9 +1,9 @@
 mod basic_stats_update;
 mod cmd_latency_clear;
-mod latency_update;
+mod prometheus_latency_update;
 mod status_update;
 
 pub use basic_stats_update::*;
 pub use cmd_latency_clear::*;
-pub use latency_update::*;
+pub use prometheus_latency_update::*;
 pub use status_update::*;
