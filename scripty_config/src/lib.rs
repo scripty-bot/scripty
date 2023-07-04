895433c8a599204a3515d9c8fbf04e1def0b8cda
Zero <zero@imaskeleton.me>
2023-07-04 15:44:39 -0600

    update the Windows compilation failure message with note about a segfault on Windows

--- scripty_config/src/lib.rs
+++ scripty_config/src/lib.rs
@@ -7,13 +7,18 @@ mod load;
 // we place all checks here that way they're tossed as early in the build as possible
 
 #[cfg(target_os = "windows")]
-compile_error!("Scripty *will not* run on Windows whatsoever. Don't even try.");
+compile_error!(
+	"\
+Scripty *will not* run on Windows whatsoever. Don't even try.\n\
+Note: one of our downstream dependencies also has a chance to segfault on Windows.\
+"
+);
 
 #[cfg(not(target_os = "linux"))]
 compile_error!(
 	"Scripty is only designed for Linux.\
  It may not run at all on other platforms.\
- If you'd like to try anyways, comment out this section of code."
+ If you'd like to try anyway, comment this section of code."
 );
 
 pub use cfg::*;
