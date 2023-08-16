de49584c9cc1bfb0031f61c851cb5d5484f2c6b3
Zero <zero@imaskeleton.me>
2023-08-15 21:30:08 -0600

    Upgrade sqlx to 0.7

--- scripty_utils/src/lib.rs
+++ scripty_utils/src/lib.rs
@@ -6,14 +6,12 @@ use tokio::sync::Mutex;
 #[macro_use]
 extern crate tracing;
 
-mod block_in_place;
 mod embed_pagination;
 mod hash_user_id;
 mod hex_vec;
 pub mod latency;
 mod separate_num;
 
-pub use block_in_place::block_in_place;
 pub use embed_pagination::do_paginate;
 pub use hash_user_id::hash_user_id;
 pub use hex_vec::vec_to_hex;
