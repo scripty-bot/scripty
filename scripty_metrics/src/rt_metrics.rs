e64835d8919ace55ce87a1f1ca3f28e52d1cb86d
Niko <zero@imaskeleton.me>
2023-12-03 10:26:05 -0700

    Update runtime metrics

--- scripty_metrics/src/rt_metrics.rs
+++ scripty_metrics/src/rt_metrics.rs
@@ -21,12 +21,18 @@ pub fn register_metrics(handle: tokio::runtime::Handle) {
 				total_park_count,
 				max_park_count,
 				min_park_count,
+				mean_poll_duration,
+				mean_poll_duration_worker_min,
+				mean_poll_duration_worker_max,
 				total_noop_count,
 				max_noop_count,
 				min_noop_count,
 				total_steal_count,
 				max_steal_count,
 				min_steal_count,
+				total_steal_operations,
+				max_steal_operations,
+				min_steal_operations,
 				num_remote_schedules,
 				total_local_schedule_count,
 				max_local_schedule_count,
@@ -45,6 +51,8 @@ pub fn register_metrics(handle: tokio::runtime::Handle) {
 				max_local_queue_depth,
 				min_local_queue_depth,
 				elapsed,
+				budget_forced_yield_count,
+				io_driver_ready_count,
 				..
 			} = interval;
 			m.runtime_metrics.workers_count.set(workers_count as i64);
@@ -53,6 +61,15 @@ pub fn register_metrics(handle: tokio::runtime::Handle) {
 				.set(total_park_count as i64);
 			m.runtime_metrics.max_park_count.set(max_park_count as i64);
 			m.runtime_metrics.min_park_count.set(min_park_count as i64);
+			m.runtime_metrics
+				.mean_poll_duration
+				.set(mean_poll_duration.as_nanos() as i64);
+			m.runtime_metrics
+				.mean_poll_duration_worker_min
+				.set(mean_poll_duration_worker_min.as_nanos() as i64);
+			m.runtime_metrics
+				.mean_poll_duration_worker_max
+				.set(mean_poll_duration_worker_max.as_nanos() as i64);
 			m.runtime_metrics
 				.total_noop_count
 				.set(total_noop_count as i64);
@@ -67,6 +84,15 @@ pub fn register_metrics(handle: tokio::runtime::Handle) {
 			m.runtime_metrics
 				.min_steal_count
 				.set(min_steal_count as i64);
+			m.runtime_metrics
+				.total_steal_operations
+				.set(total_steal_operations as i64);
+			m.runtime_metrics
+				.max_steal_operations
+				.set(max_steal_operations as i64);
+			m.runtime_metrics
+				.min_steal_operations
+				.set(min_steal_operations as i64);
 			m.runtime_metrics
 				.num_remote_schedules
 				.set(num_remote_schedules as i64);
@@ -119,6 +145,12 @@ pub fn register_metrics(handle: tokio::runtime::Handle) {
 				.min_local_queue_depth
 				.set(min_local_queue_depth as i64);
 			m.runtime_metrics.elapsed.set(elapsed.as_nanos() as i64);
+			m.runtime_metrics
+				.budget_forced_yield_count
+				.set(budget_forced_yield_count as i64);
+			m.runtime_metrics
+				.io_driver_ready_count
+				.set(io_driver_ready_count as i64);
 
 			std::thread::sleep(ONE_SECOND);
 		}
