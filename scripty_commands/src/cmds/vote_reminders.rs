00e54bcbba546bfd867eae15e9893055891ff8f2
Niko <zero@imaskeleton.me>
2023-12-26 16:22:04 -0700

    fix type mismatch

--- scripty_commands/src/cmds/vote_reminders.rs
+++ scripty_commands/src/cmds/vote_reminders.rs
@@ -8,16 +8,17 @@ pub async fn vote_reminder(ctx: Context<'_>, enabled: bool) -> Result<(), Error>
 			.await;
 
 	let db = scripty_db::get_db();
+	let hashed_user_id = scripty_utils::hash_user_id(ctx.author().id.get());
 	sqlx::query!(
 		"INSERT INTO users (user_id) VALUES ($1) ON CONFLICT ON CONSTRAINT users_pkey DO NOTHING",
-		ctx.author().id.get() as i64
+		hashed_user_id,
 	)
 	.execute(db)
 	.await?;
 	sqlx::query!(
 		"UPDATE users SET vote_reminder_disabled = $1 WHERE user_id = $2",
 		enabled,
-		ctx.author().id.get() as i64
+		hashed_user_id,
 	)
 	.execute(db)
 	.await?;
