996ce82dbe101fa2af9e965e5300bab043aa4eee
valkyrie_pilot <valk@randomairborne.dev>
2024-01-29 16:20:13 -0700

    update scripty webserver

--- scripty_webserver/src/entrypoint.rs
+++ scripty_webserver/src/entrypoint.rs
@@ -1,11 +1,15 @@
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
 pub async fn entrypoint() {
 	let cfg = scripty_config::get_config();
-	let bind_addr = cfg.bind_address.parse().expect("invalid bind address");
+	let bind_addr: SocketAddr = cfg.bind_address.parse().expect("invalid bind address");
 
 	let router = crate::endpoints::router();
 
-	axum::Server::bind(&bind_addr)
-		.serve(router.into_make_service())
+	let tcp = TcpListener::bind(bind_addr).await.unwrap();
+
+	axum::serve(tcp, router)
 		.await
 		.expect("failed to start server");
 }
