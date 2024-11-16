4b32bfde2f4c6cbbd04e0b5f44229df56505dbf7
Niko <niko@niko.lgbt>
2024-11-16 14:47:16 -0700

    Fix base URL

--- scripty_integrations/src/kiai/http_client.rs
+++ scripty_integrations/src/kiai/http_client.rs
@@ -2,6 +2,8 @@ use reqwest::{Client as ReqwestClient, StatusCode};
 
 use crate::kiai::{KiaiApiError, KiaiApiResult, KiaiPostVirtualMessage, Permissions};
 
+const BASE_API_URL: &str = "https://api.kiai.app/v1";
+
 #[derive(Debug)]
 pub struct KiaiHttpClient {
 	client: ReqwestClient,
@@ -19,10 +21,7 @@ impl KiaiHttpClient {
 		message: KiaiPostVirtualMessage,
 		guild_id: u64,
 	) -> KiaiApiResult<()> {
-		let url = format!(
-			"https://api.kiaibot.com/v1/guild/{}/virtual_message",
-			guild_id
-		);
+		let url = format!("{}/guild/{}/virtual_message", BASE_API_URL, guild_id);
 		let res = self
 			.client
 			.post(&url)
@@ -34,7 +33,7 @@ impl KiaiHttpClient {
 	}
 
 	pub async fn get_permissions(&self, guild_id: u64) -> KiaiApiResult<Permissions> {
-		let url = format!("https://api.kiaibot.com/v1/guild/{}/permissions", guild_id);
+		let url = format!("{}/guild/{}/permissions", BASE_API_URL, guild_id);
 
 		let res = self
 			.client
